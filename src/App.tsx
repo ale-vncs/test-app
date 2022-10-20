ab3de8a1ba35fe68f02569e5e38feec27ec20d52
Alessandro Santos <alessandro.vinicius@eurekalabs.com.br>
2022-10-19 21:59:57 -0300

    primeiro comit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Grid} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      container
+      sx={{
+        background: '#fff3ed',
+        height: '100vh',
+        width: '100vw'
+      }}
+    >
+      <Grid
+        item
+        container
+        direction={'row'}
+        wrap={'nowrap'}
+        columnGap={2}
+        justifyContent={'flex-end'}
+        sx={{
+          background: '#f09692',
+          height: 60
+        }}
+      >
+        <Grid item>
+          Menu 1
+        </Grid>
+        <Grid item>
+          Menu 2
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
